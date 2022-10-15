b6d403ec0bc4f3c5ac76a3845a58a35ed14ee57b
M0rG <54027806+Cedylol2002@users.noreply.github.com>
2022-10-15 17:04:48 +0200

    Added CORS Policy

--- src/main/java/com/totemware/api/controller/NewsController.java
+++ src/main/java/com/totemware/api/controller/NewsController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
-@CrossOrigin
+@CrossOrigin("http://localhost:4200")
 @RequestMapping("totem/api/v1")
 public class NewsController {
     private final NewsService newsService;
